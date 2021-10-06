5354012d4765c88504b0c254fe6ac56d6a55aae5
salvadororjuela <salvadororjuela@gmail.com>
2021-10-06 17:07:05 -0500

    Lecture 4, django and Sqlite3 first commit

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("login", views.logout_view, name="logout"),
+]
